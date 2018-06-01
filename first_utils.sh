21c3dd4de130a5b828a870eaa9560d203c099ce4
Allison Jack <allison.e.jack@gmail.com>
2018-06-01 12:30:51 -0400

    Update first_utils.sh

--- first_utils.sh
+++ first_utils.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 HOME=/Users/allisonjack/Documents/CDD_R01/FIRST
+LABEL=your_label
 MAIN=$HOME/SHAPE_ANALYSIS
 
 for ROI in L_Thal R_Thal L_Amyg R_Amyg L_Caud R_Caud L_Hipp R_Hipp; do 
 
-  first_utils --vertexAnalysis --usebvars -i $MAIN/${ROI}_onset2_CDD_ASDR.bvars -d $MAIN/onset2_CDD_ASDR.mat -o $MAIN/${ROI}_onset2_CDD_ASDR_MNI --useReconMNI 
+  first_utils --vertexAnalysis --usebvars -i $MAIN/${ROI}_${LABEL}.bvars -d $MAIN/${LABEL}.mat -o $MAIN/${ROI}_${LABEL}_MNI --useReconMNI 
 
 done
