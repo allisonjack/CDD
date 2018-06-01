df28285b7f40b1095fc8d961bac3cad18214527c
Allison Jack <allison.e.jack@gmail.com>
2018-06-01 11:21:11 -0400

    Create bvars_concat_list.sh

---
+++ bvars_concat_list.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+HOME=/Users/allisonjack/Documents/CDD_R01
+IDLIST=$HOME/FIRST/SHAPE_ANALYSIS/IDs_onset2_CDD_ASDR.txt
+
+for ROI in L_Thal R_Thal L_Amyg R_Amyg L_Caud R_Caud L_Hipp R_Hipp; do
+
+      while read -r ID GRP || [[ -n "$ID" ]]; do
+
+        INPUT=$HOME/FIRST/${ID}-${ROI}_first.bvars
+        printf "$INPUT " >> bvarsin_${ROI}_tmp.txt
+
+      done < $IDLIST
+
+  BVARSIN=$(cat bvarsin_${ROI}_tmp.txt)
+
+  OUTDIR=$HOME/FIRST/SHAPE_ANALYSIS
+  mkdir -p $OUTDIR
+
+  concat_bvars $OUTDIR/${ROI}_onset2_CDD_ASDR.bvars $BVARSIN
+
+  rm *tmp*
+done
